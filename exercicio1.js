52f2f55b9f2079a181a9396ef3059d914a477d09
Pedro Regenes <phregenes@gmail.com>
2019-10-29 00:28:43 -0300

    1.0

---
+++ exercicio1.js
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title>curso javascript</title></title>
+	</head>
+	<body>
+		<script type="text/javascript">
+
+			const endereco = {
+				rua: "Rua dos pinheiros",
+ 				numero: 1293,
+				bairro: "Centro",
+ 				cidade: "São Paulo",
+ 				uf: "SP"
+ 			};
+
+ 		console.log (" o usuario mora em " + endereco.cidade + "/" + endereco.uf + " no bairro " + endereco.rua + " na rua " + endereco.rua + " com numero " + endereco.numero
+ 			);
+
+
+		
+		</script>
+		
+	</body>
+	</html>
+
+
---
+++ exercicio2.js
@@ -0,0 +1,26 @@
+
+<html>
+  <head>
+    <title>curso javascript</title></title>
+  </head>
+  <body>
+    <script type="text/javascript">
+var j=32, msg="";
+while (j<=321){
+     if(j ==321){
+       msg +=j;
+       break;
+     }
+     if(j%2 !=0){
+       msg += "";
+     }else{
+       msg += j + ", ";
+    }
+    j++;
+  };
+
+    
+    </script>
+    
+  </body>
+  </html>
